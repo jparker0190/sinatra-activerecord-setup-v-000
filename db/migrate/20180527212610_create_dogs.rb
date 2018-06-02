e4ed14bd29dba3cfb6f7e348e39fa2d22e01bc81
Joseph Parker <jparker010190@gmail.com>
2018-06-02 14:42:17 +0000

    automatically backed up by learn

--- db/migrate/20180527212610_create_dogs.rb
+++ db/migrate/20180527212610_create_dogs.rb
@@ -1,6 +1,10 @@
 class CreateDogs < ActiveRecord::Migration
 
+<<<<<<< HEAD
   def change
+=======
+  def change 
+>>>>>>> fd18103212cf9a90069ac34195af70f464c2385a
     create_table :dogs do |t|
       t.string :name
       t.string :breed
